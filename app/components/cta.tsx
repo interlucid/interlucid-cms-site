c8364c45ba8de8cf8c4360f3f56ba15f5f4127d4
intcreator <intcreator@users.noreply.github.com>
2025-02-21 16:24:58 -0800

    finish upgrading Tailwind to v4

--- app/components/cta.tsx
+++ app/components/cta.tsx
@@ -4,7 +4,7 @@ import type { Props } from 'react';
 export function Cta(props: Props<string>) {
     return (
         <>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{props.children}</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm">{props.children}</button>
         </>
     );
 }
